c3886fc33214db79da633daf2321a05533e0330d
Sebastian Wolff <sebastian.wolff@nyu.edu>
2023-03-02 19:44:55 -0500

    benchmarking with fallback

--- reduced.sh
+++ reduced.sh
@@ -22,6 +22,40 @@ BENCHMARKS=(
 mkdir -p "${LOGS}"
 rm -f -- "${LOGS}"/*
 
+exec_benchmark() {
+  name=$1
+  mode=$2
+  output=$3
+  cmd=$4
+  touch "${output}"
+  {
+    echo "${name}"
+    echo "${mode}"
+    echo "${cmd}"
+    date
+    echo "##################################"
+    echo ""
+    echo ""
+    time ${cmd}
+  } > "${output}" 2>&1
+  return $?
+}
+
+run_benchmark() {
+  name=$1
+  echo -n "    - Running --old analysis: ${name} "
+  exec_benchmark "$1" "$2" "$3" "$4"
+  if [ $? -eq 0 ]
+  then echo "✓"
+  else
+    exec_benchmark "$1" "$2" "$3" "$4"
+    if [ $? -eq 0 ]
+    then echo "✓"
+    else echo "✗"
+    fi
+  fi
+}
+
 for (( iteration = 0; iteration < ITER; iteration++ )); do
   echo "# Iteration $((iteration + 1))/${ITER}"
   for (( index = 0; index < ${#BENCHMARKS[@]}; index++ )); do
@@ -33,42 +67,9 @@ for (( iteration = 0; iteration < ITER; iteration++ )); do
     output_new="${LOGS}/${name}-new-${iteration}.txt"
     cmd_old="${EXECUTABLE} --old ${flags} ${input}"
     cmd_new="${EXECUTABLE} --new ${flags} ${input}"
-    # running original analysis
-    touch "${output_old}"
-    echo -n "    - Running --old analysis: ${name} "
-    {
-      echo "${name}"
-      echo "old"
-      echo "${cmd_old}"
-      date
-      echo "##################################"
-      echo ""
-      echo ""
-      time ${cmd_old}
-    } > "${output_old}" 2>&1
-    if [ $? -eq 0 ]
-    then echo "✓"
-    else echo "✗"
-    fi
-    # running new analysis
-    touch "${output_old}"
-    echo -n "    - Running --new analysis: ${name} "
-    {
-      echo "${name}"
-      echo "new"
-      echo "${cmd_new}"
-      date
-      echo "##################################"
-      echo ""
-      echo ""
-      time ${cmd_new}
-    } > "${output_new}" 2>&1
-    if [ $? -eq 0 ]
-    then echo "✓"
-    else echo "✗"
-    fi
-    # ${EXECUTABLE} ${input} >> "${output}"
-    done
+    run_benchmark "${name}" "old" "${output_old}" "${cmd_old}"
+    run_benchmark "${name}" "new" "${output_new}" "${cmd_new}"
+  done
 done
 echo "[done]"
 ${PRINTER} "${LOGS}"
